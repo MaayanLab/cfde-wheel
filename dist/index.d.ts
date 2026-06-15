8d4db0a1ce0dc372f7912879ef8ac42ca1a78f1c
John Erol Evangelista <je.evangelista04@gmail.com>
2026-06-15 15:43:27 -0400

    updated wheel exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,14 @@
 import * as react_jsx_runtime from 'react/jsx-runtime';
 
-interface dccType {
-    id: string;
-    label: string;
-    homepage: string;
-    icon: string;
-    description?: string;
-}
+declare function InteractiveNavComponent({ dccs, handleClose, new_window }: {
+    dccs: Array<any>;
+    handleClose: any;
+    new_window?: boolean;
+}): react_jsx_runtime.JSX.Element;
+
 declare const CFDEWheel: ({ button, new_window }: {
     button?: boolean;
     new_window?: boolean;
 }) => react_jsx_runtime.JSX.Element;
 
-export { type dccType, CFDEWheel as default };
+export { CFDEWheel, InteractiveNavComponent, CFDEWheel as default };
---
+++ src/wheel.tsx
@@ -0,0 +1,72 @@
+"use client"
+import InteractiveNavComponent from "./interactive";
+import { useEffect, useRef, useState } from "react";
+import Modal from '@mui/material/Modal';
+import Fab from '@mui/material/Fab';
+import Button from '@mui/material/Button';
+import dccs_default from './dccs.json'
+import { Tooltip } from "@mui/material";
+export * from "./interactive"
+export interface dccType {
+	id: string
+	label: string
+	homepage: string
+	icon: string
+	description?: string 
+}
+
+const FabComponent = ({children, ...props}: {children: React.ReactNode}) => (
+	<Fab {...props} sx={{
+		position: 'fixed',
+		bottom: 50,
+		right: 50,
+		height: 70,
+		width: 70,
+		backgroundColor: '#FFF',
+		padding: 0
+	}}>{children}</Fab>
+)
+
+
+const CFDEWheel = ({button, new_window}: {button?:boolean, new_window?:boolean}) => {
+	const [dccs, setDccs] = useState<Array<dccType>>([])
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+	const Wrapper = button ? Button: FabComponent
+	useEffect(()=>{
+		const fetch_dccs = async () => {
+			try {
+				console.log("Fetching")
+				const r = await fetch("https://raw.githubusercontent.com/MaayanLab/cfde-wheel/refs/heads/main/src/dccs.json")
+				setDccs(await r.json())
+			} catch (error) {
+				setDccs(dccs_default)
+			}
+			
+		}
+		fetch_dccs()
+	}, [])
+	
+	return (
+		<div>
+			<Tooltip title="Open CFDE Wheel">
+				<Wrapper onClick={handleOpen}>
+					<img src="https://cfde-drc.s3.us-east-2.amazonaws.com/assets/img/cfde_unified_icon.svg" alt={'nav-but'} width={120} height={120}/>
+				</Wrapper>
+			</Tooltip>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<>
+					<InteractiveNavComponent dccs={dccs} handleClose={handleClose} new_window={new_window}/>
+				</>
+			</Modal>
+		</div>
+	)
+}
+
+export default CFDEWheel
